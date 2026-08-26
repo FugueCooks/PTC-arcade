6f6023e7c50d2611a0968317c25b8eb126fa1b9d
FugueCooks <FugueCooks@users.noreply.github.com>
2026-08-26 12:26:59 -1000

    Bust PS2 mobile control cache chain

--- world/world-manager.js
+++ world/world-manager.js
@@ -1,5 +1,5 @@
 import { AudioManager } from './audio-manager.js';
-import { EnvironmentManager } from './environment-manager.js?v=megaman-room-relocate-1';
+import { EnvironmentManager } from './environment-manager.js?v=ps2-touch-1';
 import { LightingManager } from './lighting-manager.js';
 import { ObjectInteractionManager } from './object-interaction-manager.js';
 import { ParticleManager } from './particle-manager.js';
